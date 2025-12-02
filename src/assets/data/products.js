60360bd4a2549682511a1bcce11654bfeb8ee001
Pooja Naik <136612253+poojanaik07@users.noreply.github.com>
2025-12-02 22:52:56 +0530

    Add files via upload

---
+++ src/assets/data/products.js
@@ -0,0 +1,26 @@
+import productImg01 from "../images/sm_concrete_acc1.jpg";
+
+
+const products = [
+
+  {
+    id: "01",
+    productName: "ACC Cement Concrete-Structural Material",
+    imgUrl: productImg01,
+    category: "concrete",
+    price: 193,
+    shortDesc:
+      "ACC Limited Concrete",
+    description:
+      "Net Quantity 50kg-Perfect for all concrete applications – foundations, columns, beams and roofs",
+    reviews: [
+      {
+        rating: 4.7,
+        text: "Lorem ipsum",
+      },
+    ],
+    avgRating: 4.5,
+  },
+];
+
+export default products;
---
+++ src/assets/data/serviceData.js
@@ -0,0 +1,28 @@
+const serviceData = [
+  {
+    icon: "ri-truck-line",
+    title: "Free Shipping",
+    subtitle: "Reliable and fast delivery",
+    bg: "#d1cee0",
+  },
+  {
+    icon: "ri-refresh-line",
+    title: "Easy Returns",
+    subtitle: "Exchange and return available",
+    bg: "#ceebe9",
+  },
+  {
+    icon: "ri-secure-payment-line",
+    title: "Secure Payment",
+    subtitle: "Do payment securely",
+    bg: "#d1cee0",
+  },
+  {
+    icon: "ri-exchange-dollar-line",
+    title: " Back Guarantee",
+    subtitle: "100% back guarantee",
+    bg: "#d6e5fb",
+  },
+];
+
+export default serviceData;
